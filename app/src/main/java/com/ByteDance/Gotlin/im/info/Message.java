3e112a3b8b0047159b31a886a17e70003a7a04de
horsefoxhj <1349919459@qq.com>
2022-06-12 22:43:11 +0800

    完成聊天气泡绘制

---
+++ app/src/main/java/com/ByteDance/Gotlin/im/info/Message.java
@@ -0,0 +1,12 @@
+package com.ByteDance.Gotlin.im.info;
+
+/**
+ * @author: Hx
+ * @date: 2022年06月12日 19:03
+ */
+public class Message {
+
+    public static final int TYPE_RECEIVED = 0;
+    public static final int TYPE_SENT = 1;
+
+}
